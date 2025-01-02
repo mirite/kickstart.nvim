4a22acce5605153b7955f2f614b934d56d803d21
Jesse Conner <jconner90@gmail.com>
2025-01-02 15:43:06 -0500

    Bump up prettier timeout

--- lua/custom/plugins/formatter.lua
+++ lua/custom/plugins/formatter.lua
@@ -22,7 +22,7 @@ return {
       format_on_save = {
         lsp_fallback = true,
         async = false,
-        timeout_ms = 1000,
+        timeout_ms = 10000,
       },
     }
 
@@ -30,7 +30,7 @@ return {
       conform.format {
         lsp_fallback = true,
         async = false,
-        timeout_ms = 1000,
+        timeout_ms = 10000,
       }
     end, { desc = 'Format file or range (in visual mode)' })
   end,
