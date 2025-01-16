98b717f28109a89feac69d9e09c6e8ebca6207c9
Jesse Conner <jconner90@gmail.com>
2025-01-16 11:47:30 -0500

    Turn spelling on by default

--- init.lua
+++ init.lua
@@ -159,10 +159,8 @@ vim.opt.scrolloff = 20
 vim.opt.spell = true
 vim.opt.spelllang = 'en_ca'
 
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.opt.confirm = true
+vim.opt.spell = true
+vim.opt.spelllang = 'en_ca'
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
