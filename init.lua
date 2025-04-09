e7a0e780eb14d8c86db74fd380326ce1e55aca29
Theo P. <63016528+theopn@users.noreply.github.com>
2025-04-09 11:01:57 -0400

    Change LSP Keybindings to Match the Default `gr` Bindings Introduced in Neovim 0.11 (#1427)
    * refactor: change LSP keybindings to the default gr bindings introduced in 0.11
    
    * refactor: modify existing LSP functions to follow  convention

--- init.lua
+++ init.lua
@@ -338,11 +338,7 @@ require('lazy').setup({
 
       -- Document existing key chains
       spec = {
-        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
-        { '<leader>d', group = '[D]ocument' },
-        { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
-        { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
